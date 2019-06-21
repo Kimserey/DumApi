de1bb81738d495583948553cdc4e3df370439ea6
kimserey lam <kimserey.lam@jet.com>
2019-06-21 12:26:12 +0100

    Init

---
+++ DumApi/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DumpApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<int> Get(int id)
+        {
+            return id;
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+            return value;
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<int> Delete(int id)
+        {
+            return id;
+        }
+    }
+}
